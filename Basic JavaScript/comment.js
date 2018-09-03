d0f01c3557c99077c87958bf70ddd80718932775
Kartikey Tripathi <kartikey.tripathi.37@gmail.com>
2018-09-03 11:57:50 +0530

    Comment Your JavaScript Code

---
+++ Basic JavaScript/comment.js
@@ -0,0 +1,16 @@
+/* 
+Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave 
+notes to yourself and to other people who will later need to figure out what that code does.
+*/
+
+/*
+As you write code, you should regularly add comments to clarify the function of parts of your code.
+Good commenting can help communicate the intent of your code—both for others and for your future self.
+*/
+
+// This is an in-line comment
+
+
+/* This is a
+multi-line comment */
+
