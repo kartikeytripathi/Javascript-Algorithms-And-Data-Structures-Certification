b6c583f08926e1ba5311e8df305cd63082e04b48
Kartikey Tripathi <kartikey.tripathi.37@gmail.com>
2018-09-04 16:23:30 +0530

    Escape Sequences in Strings

---
+++ Basic JavaScript/escapeSequenceStr.js
@@ -0,0 +1,17 @@
+/* -----------------------------------------------------------------------------------------------------------------------
+
+Quotes are not the only characters that can be escaped inside a string. There are two reasons to use escaping 
+characters: First is to allow you to use characters you might not otherwise be able to type out, such as a backspace.
+Second is to allow you to represent multiple quotes in a string without JavaScript misinterpreting what you mean
+
+Assign the following three lines of text into the single variable myStr using escape sequences.
+
+    FirstLine
+        \SecondLine
+    ThirdLine
+
+-------------------------------------------------------------------------------------------------------------------------*/
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+
